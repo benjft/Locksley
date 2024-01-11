24295cf20d42e866bac54dd5cf45e8d2c16d5edd
Benedict Thompson <benjfthompson@gmail.com>
2024-01-11 21:30:05 +0000

    Refactor AndroidLogger

--- Locksley/Platforms/Android/Services/AndroidLogger.cs
+++ Locksley/Platforms/Android/Services/AndroidLogger.cs
@@ -1,7 +1,4 @@
-﻿using Java.Lang;
-using Microsoft.Extensions.Logging;
-using Enum = System.Enum;
-using Exception = System.Exception;
+﻿using Microsoft.Extensions.Logging;
 using Logcat = Android.Util.Log;
 
 namespace Locksley.Services;
@@ -16,21 +13,18 @@ public class AndroidLogger(string category) : ILogger {
         Func<TState, Exception?, string> formatter) {
         
         var message = formatter(state, exception);
-        var throwable = exception != null ? Throwable.FromException(exception) : null;
-
+        var throwable = exception != null ? Java.Lang.Throwable.FromException(exception) : null;
         switch (logLevel) {
-#pragma warning disable CS8604 // Possible null reference argument.
-            case LogLevel.Trace:       Logcat.Verbose(category, throwable, message); break;
-            case LogLevel.Debug:       Logcat.Debug(category, throwable, message); break;
-            case LogLevel.Information: Logcat.Info(category, throwable, message); break;
-            case LogLevel.Warning:     Logcat.Warn(category, throwable, message); break;
-            case LogLevel.Error:       Logcat.Error(category, throwable, message); break;
-            case LogLevel.Critical:    Logcat.Wtf(category, throwable, message); break;
-#pragma warning restore CS8604 // Possible null reference argument.
+            case LogLevel.Trace:       Logcat.Verbose(category, throwable!, message); break;
+            case LogLevel.Debug:       Logcat.Debug(category, throwable!, message); break;
+            case LogLevel.Information: Logcat.Info(category, throwable!, message); break;
+            case LogLevel.Warning:     Logcat.Warn(category, throwable!, message); break;
+            case LogLevel.Error:       Logcat.Error(category, throwable!, message); break;
+            case LogLevel.Critical:    Logcat.Wtf(category, throwable!, message); break;
             
             case LogLevel.None: break; 
             
-            default: throw new IndexOutOfRangeException($"LogLevel {Enum.GetName(logLevel)} didn't match any known value");
+            default: throw new IndexOutOfRangeException($"LogLevel {logLevel} didn't match any known value");
         }
     }
 
